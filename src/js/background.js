414e75fe675aef2e8ecda024cc199187cf77a8bc
Jokke <jonnejokke2@gmail.com>
2016-04-21 18:18:47 +0300

    Fixed channel click, when offline text was a link

--- src/js/background.js
+++ src/js/background.js
@@ -8,7 +8,7 @@
 	 * OPTIONS
 	 * userName = channel
 	 * key = https://www.livecoding.tv/rss/USERNAME/followed/?key=KEY
-	 * useNotifications = popup chrome notification when followed streams goes live.
+	 * useNotifications = popup chrome notification when followed streams goes live. (true/false)
 	 */
 	var userName = '';
 	var key = '';
@@ -19,38 +19,26 @@
 		updateStreams();
 
 		$('body').on('click', 'td', function() {
-			chrome.tabs.create({
-				url: 'https://www.livecoding.tv/' + $(this).attr('channel')
-			});
-			return false;
+			if ($('tr').hasClass('offline') !== true) {
+				chrome.tabs.create({
+					url: 'https://www.livecoding.tv/' + $(this).attr('channel')
+				});
+				return false;
+			}
 		});
 
-		$('.btn-update').click(function() {
-			getLiveChannels();
-		});
+		$('.btn-update').bind('click', getLiveChannels);
 	});
 
 	function getLiveChannels() {
 		$.ajax({
 			url: 'https://www.livecoding.tv/rss/' + userName + '/followed/?key=' + key,
 			cache: false
-		}).done(function(data) {
+		})
+		.done(function(data) {
 			var tmp = xml2json(data);
 
-			$.each(tmp.rss.channel.item, function(key, value) {
-				if (isLive(value.description)) {
-					if ($.inArray(value.title, channels) === -1) {
-						channels.push(value.title);
-						
-						if (useNotifications)
-							setNotificationOptions(value.title);
-					}
-				} else {
-					if ($.inArray(value.title, channels) !== -1) {
-						channels.splice(channels.indexOf(value.title, 1));
-					}
-				}
-			});
+			parseChannelData(tmp.rss.channel.item);
 
 			if (channels.length === 0 && $('tr').hasClass('offline') !== true) {
 				$('<tr />').addClass('offline').appendTo('tbody');
@@ -65,15 +53,34 @@
 			}
 
 			var text = '' + channels.length;
-			var color = [190, 190, 190, 230];
 
-			updateBadge(text, color);
+			updateBadge(text);
+		});
+	}
+
+	function parseChannelData(data) {
+		$.each(data, function(key, value) {
+			var channelName = value.title;
+			var channelStatus = value.description;
+
+			if (isLive(channelStatus)) {
+				if ($.inArray(channelName, channels) === -1) {
+					addNewChannel(channelName);
+					
+					if (useNotifications)
+						showNotification(channelName);
+				}
+			} else {
+				if ($.inArray(channelName, channels) !== -1)
+					removeChannel(channelName);
+			}
 		});
 	}
 
 	function updateStreams() {
 		setInterval(function() {
 			getLiveChannels();
+			console.warn('New data requested');
 		}, 5000);
 	}
 
@@ -107,9 +114,9 @@
 		}
 	}
 
-	function updateBadge(str, color) {
+	function updateBadge(str) {
 		chrome.browserAction.setBadgeBackgroundColor({
-			color: color
+			color: [190, 190, 190, 230]
 		});
 
 		chrome.browserAction.setBadgeText({
@@ -118,7 +125,7 @@
 	}
 
 
-	function setNotificationOptions(channelName) {
+	function showNotification(channelName) {
 		var options = {
 			title: 'LCTV - Live Channels',
 			priority: 0,
@@ -134,6 +141,14 @@
 
 	function onNotification(notificationId) {
 		notification = notificationId;
+	}
+
+	function addNewChannel(channel) {
+		channels.push(channel);
+	}
+
+	function removeChannel(channel) {
+		channels.splice(channels.indexOf(channel, 1));
 	}
 
 	function isLive(str) {
